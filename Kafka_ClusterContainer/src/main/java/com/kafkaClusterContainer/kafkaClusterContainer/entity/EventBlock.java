907aa0d34c3fff31337b07f164cf8fa5bf685712
DotBlossom <calarito98@gmail.com>
2024-11-05 10:57:22 +0900

    eventBlockCreated

---
+++ Kafka_ClusterContainer/src/main/java/com/kafkaClusterContainer/kafkaClusterContainer/entity/EventBlock.java
@@ -0,0 +1,52 @@
+package com.kafkaClusterContainer.kafkaClusterContainer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventBlock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long eventBlockId;
+
+    // check only BinaryType , just has been completed?
+    private String totalEventBlockChecker;
+
+
+    // eventTracker Depth
+    private int mainDepth;
+    private int currentDepth;
+
+    // prev-curr supporting Indications
+
+    private String prevEventTransaction;
+    private String currentEventTransaction;
+
+
+    // 16 hexType or binary snapshot.
+    private String transactionIndicator;
+
+
+    @Override
+    public String toString() {
+        return "eventBlock : {"
+                + "eventBlockId:" + eventBlockId +
+                "totalEventBlockChecker:" + totalEventBlockChecker +
+                "depth:" + mainDepth + " / " + currentDepth +
+                "EventTransaction: {" + "prev :" + prevEventTransaction +
+                "curr :" + currentEventTransaction + " } " +
+                "transactionIndicator: " + transactionIndicator +
+                "}";
+    }
+
+}
+
